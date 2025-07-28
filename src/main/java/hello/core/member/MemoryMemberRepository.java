6010dd97370633620af71a5ba29aeca0ebdc647d
JJung0 <junghwan__@naver.com>
2025-07-28 19:26:33 +0900

    FEAT: 회원 도메인 설계

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -7,6 +7,7 @@ public class MemoryMemberRepository implements MemberRepository {
 
     // 동시성 문제 해결 -> ConcurrentHashMap
     // 내부적으로 락(Lock)을 사용하지만, 전체가 아닌 부분적으로 걸림 (Segment, Bucket 단위)
+
     private static Map<Long, Member> store = new HashMap<>();
 
     @Override

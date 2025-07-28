9c5158dd7182428b5a1b3d26286291b56cf4134c
JJung0 <junghwan__@naver.com>
2025-07-28 19:25:48 +0900

    FEAT:회원 도메인 설계

---
+++ src/main/java/hello/core/member/Grade.java
@@ -0,0 +1,9 @@
+package hello.core.member;
+
+/**
+ * 등급 (Enum 타입)
+ */
+public enum Grade {
+    BASIC,
+    VIP
+}
---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // 동시성 문제 해결 -> ConcurrentHashMap
+    // 내부적으로 락(Lock)을 사용하지만, 전체가 아닌 부분적으로 걸림 (Segment, Bucket 단위)
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
